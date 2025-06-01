07c6cf895ab9c18262cb607ac0642771143645af
Piotr Pijanowski <piotr.pijanowski5@gmail.com>
2025-06-01 21:28:24 +0200

    push

--- Semestr 4/Sieci Komputerowe/Webserver/server.c
+++ Semestr 4/Sieci Komputerowe/Webserver/server.c
@@ -11,6 +11,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/select.h>
+#include <limits.h>  // dla PATH_MAX
 
 #define BUF_SIZE 8192            // Rozmiar bufora danych
 #define KEEP_ALIVE_TIMEOUT 1     // Czas oczekiwania na kolejne żądanie (sekundy)
@@ -30,17 +31,17 @@ const char *get_mime_type(const char *path)
 }
 
 // Wysyła odpowiedź HTTP z nagłówkiem i treścią (jeśli obecna)
-void send_response(int client_fd, int status, const char *status_text, const char *content_type, const char *body)
+void send_response(int client_fd, int status, const char *status_text, const char *content_type, const char *body, int keep_alive)
 {
     char header[1024];
-    int length = body ? strlen(body) : 0;
+    int length = body ? (int)strlen(body) : 0;
     snprintf(header, sizeof(header),
              "HTTP/1.1 %d %s\r\n"
              "Content-Type: %s\r\n"
              "Content-Length: %d\r\n"
-             "Connection: close\r\n"
+             "Connection: %s\r\n"
              "\r\n",
-             status, status_text, content_type, length);
+             status, status_text, content_type, length, keep_alive ? "keep-alive" : "close");
 
     if (send(client_fd, header, strlen(header), 0) < 0) perror("send");
     if (body && send(client_fd, body, length, 0) < 0) perror("send body");
@@ -52,7 +53,7 @@ void send_file(int client_fd, const char *path)
     int fd = open(path, O_RDONLY);
     if (fd < 0)
     {
-        send_response(client_fd, 404, "Not Found", "text/html; charset=utf-8", "<h1>404 Not Found</h1>");
+        send_response(client_fd, 404, "Not Found", "text/html; charset=utf-8", "<h1>404 Not Found</h1>", 0);
         return;
     }
 
@@ -60,24 +61,22 @@ void send_file(int client_fd, const char *path)
     if (fstat(fd, &st) != 0)
     {
         close(fd);
-        send_response(client_fd, 500, "Internal Server Error", "text/html; charset=utf-8", "<h1>500 Internal Server Error</h1>");
+        send_response(client_fd, 500, "Internal Server Error", "text/html; charset=utf-8", "<h1>500 Internal Server Error</h1>", 0);
         return;
     }
     int size = st.st_size;
     const char *mime = get_mime_type(path);
 
-    // Wysyłanie nagłówka HTTP
     char header[1024];
     snprintf(header, sizeof(header),
              "HTTP/1.1 200 OK\r\n"
              "Content-Type: %s\r\n"
              "Content-Length: %d\r\n"
-             "Connection: close\r\n"
+             "Connection: keep-alive\r\n"
              "\r\n",
              mime, size);
     if (send(client_fd, header, strlen(header), 0) < 0) perror("send header");
 
-    // Wysyłanie zawartości pliku
     char buffer[4096];
     ssize_t n;
     while ((n = read(fd, buffer, sizeof(buffer))) > 0)
@@ -91,17 +90,15 @@ void send_file(int client_fd, const char *path)
     close(fd);
 }
 
-// Obsługuje pojedynczego klienta (obsługa wielu zapytań w jednym połączeniu)
 void handle_client(int client_fd, const char *base_dir) {
     char buffer[BUF_SIZE];
     char method[8], path[1024], protocol[16];
     char hostname[256];
-    char domain_dir[2048];
-    char fullpath[2048];
-    char real_fullpath[2048];
-    char real_base[2048];
+    char domain_dir[PATH_MAX];
+    char fullpath[PATH_MAX];
+    char real_fullpath[PATH_MAX];
+    char real_base[PATH_MAX];
 
-    // Ustawienie timeoutu keep-alive
     struct timeval timeout;
     timeout.tv_sec = KEEP_ALIVE_TIMEOUT;
     timeout.tv_usec = 0;
@@ -111,76 +108,72 @@ void handle_client(int client_fd, const char *base_dir) {
         FD_ZERO(&fds);
         FD_SET(client_fd, &fds);
 
-        // Czekaj na dane od klienta z timeoutem
         int ready = select(client_fd + 1, &fds, NULL, NULL, &timeout);
         if (ready <= 0) break;
 
-        // Odbierz dane
         ssize_t received = recv(client_fd, buffer, BUF_SIZE - 1, 0);
         if (received <= 0) break;
 
         buffer[received] = '\0';
         printf("DEBUG: Otrzymano zapytanie:\n%s\n", buffer);
 
-        // Parsowanie linii żądania (metoda, ścieżka, protokół)
         if (sscanf(buffer, "%7s %1023s %15s", method, path, protocol) != 3) {
-            send_response(client_fd, 501, "Not Implemented", "text/html; charset=utf-8", "<h1>501 Not Implemented</h1>");
+            send_response(client_fd, 501, "Not Implemented", "text/html; charset=utf-8", "<h1>501 Not Implemented</h1>", 0);
             break;
         }
 
-        // Obsługiwane tylko zapytania GET
         if (strcmp(method, "GET") != 0) {
-            send_response(client_fd, 501, "Not Implemented", "text/html; charset=utf-8", "<h1>501 Not Implemented</h1>");
+            send_response(client_fd, 501, "Not Implemented", "text/html; charset=utf-8", "<h1>501 Not Implemented</h1>", 0);
             break;
         }
 
-        // Odczytanie nagłówka Host
         char *host = strstr(buffer, "\nHost:");
         if (!host) host = strstr(buffer, "\nhost:");
         if (!host) {
-            send_response(client_fd, 501, "Not Implemented", "text/html; charset=utf-8", "<h1>501 Host header missing</h1>");
+            send_response(client_fd, 501, "Not Implemented", "text/html; charset=utf-8", "<h1>501 Host header missing</h1>", 0);
             break;
         }
         host += 6;
         while (*host == ' ') host++;
         sscanf(host, "%255[^:\r\n]", hostname);
 
-        // Zbudowanie ścieżki do zasobu
         snprintf(domain_dir, sizeof(domain_dir), "%s/%s", base_dir, hostname);
         snprintf(fullpath, sizeof(fullpath), "%s%s", domain_dir, path);
 
-        // Obsługa przekierowania z katalogu (dodanie /index.html)
         struct stat st;
         if (stat(fullpath, &st) == 0 && S_ISDIR(st.st_mode)) {
             char location[1024];
-            if (path[strlen(path) - 1] != '/')
-                snprintf(location, sizeof(location), "HTTP/1.1 301 Moved Permanently\r\nLocation: %s/index.html\r\n\r\n", path);
-            else
-                snprintf(location, sizeof(location), "HTTP/1.1 301 Moved Permanently\r\nLocation: %sindex.html\r\n\r\n", path);
-            send(client_fd, location, strlen(location), 0);
+            if (strlen(path) < 900) {
+                if (path[strlen(path) - 1] != '/')
+                    snprintf(location, sizeof(location), "HTTP/1.1 301 Moved Permanently\r\nLocation: %s/index.html\r\nConnection: keep-alive\r\n\r\n", path);
+                else
+                    snprintf(location, sizeof(location), "HTTP/1.1 301 Moved Permanently\r\nLocation: %sindex.html\r\nConnection: keep-alive\r\n\r\n", path);
+                send(client_fd, location, strlen(location), 0);
+            } else {
+                send_response(client_fd, 414, "Request-URI Too Long", "text/html; charset=utf-8", "<h1>414 URI Too Long</h1>", 0);
+            }
             continue;
         }
 
-        // Sprawdzenie ścieżki rzeczywistej (ochrona przed ../)
-        realpath(domain_dir, real_base);
-        realpath(fullpath, real_fullpath);
+        if (!realpath(domain_dir, real_base) || !realpath(fullpath, real_fullpath)) {
+            send_response(client_fd, 500, "Internal Server Error", "text/html; charset=utf-8", "<h1>500 Internal Server Error</h1>", 0);
+            continue;
+        }
+
         printf("DEBUG: fullpath = %s\n", real_fullpath);
         printf("DEBUG: base_dir = %s\n", real_base);
 
         if (strncmp(real_fullpath, real_base, strlen(real_base)) != 0) {
-            send_response(client_fd, 403, "Forbidden", "text/html; charset=utf-8", "<h1>403 Forbidden</h1>");
+            send_response(client_fd, 403, "Forbidden", "text/html; charset=utf-8", "<h1>403 Forbidden</h1>", 0);
             continue;
         }
 
-        // Wysyłanie pliku
         send_file(client_fd, real_fullpath);
 
-        // Sprawdzenie nagłówka Connection
         if (strstr(buffer, "Connection: close") || strstr(buffer, "connection: close")) {
             break;
         }
 
-        // Reset timeoutu
         timeout.tv_sec = KEEP_ALIVE_TIMEOUT;
         timeout.tv_usec = 0;
     }
--- Semestr 4/Sieci Komputerowe/Webserver/server.h
+++ Semestr 4/Sieci Komputerowe/Webserver/server.h
@@ -3,7 +3,7 @@
 
 void handle_client(int client_fd, const char *base_dir);
 const char *get_mime_type(const char *path);
-void send_response(int client_fd, int status, const char *status_text, const char *content_type, const char *body);
+void send_response(int client_fd, int status, const char *status_text, const char *content_type, const char *body, int keep_alive);
 void send_file(int client_fd, const char *path);
 
 #endif
